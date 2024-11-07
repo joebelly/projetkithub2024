1ec145c2af51761bca2c3d8d9ee6d75b669aab65
travail2 <lievinira8@gmail.com>
2024-11-07 18:09:20 +0200

    javascript2etape

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log("hello word");
+
+for (let compteur = 0; compteur < 3; compteur = compteur + 1) {
+    console.log(compteur)
+}
+
+// entame la boucle for
+for (let i = 0; i < 3; i++) {
+    console.log(i)
+}
+//entame le boucle "while"
+let i = 0
+while (i < 3) {
+    console.log(i)
+    i++
+}
