918ad483d47d6f73a6b250ca8c0005bbdb4a7567
travail2 <lievinira8@gmail.com>
2024-11-07 18:46:37 +0200

    correction

--- jscript/l.js
+++ jscript/l.js
@@ -26,3 +26,191 @@ placesDejaVendues. Si j’avais écrit placesDejaVendues = 10, cela aurait écra
 let placesDejaVendues = 150
 placesDejaVendues += 10
 
+let monAge = 42
+console.log(monAge)
+
+/**javascript */
+
+const monPrenom = "David"
+
+
+const entreesPremiereSemaine = 1000
+const entreesDeuxiemeSemaine = 2000
+const totalEntrees = entreesPremiereSemaine + entreesDeuxiemeSemaine
+
+console.log(totalEntrees)
+
+/* Et voilà ! Vous avez fait votre première addition en JavaScript, félicitations 
+! 🥳 Le principe est le même pour les autres opérations mathématiques qui auront chacun un opérateur : les soustractions  -  ,  les multiplications  *  , les divisions  /  .
+Imaginez maintenant un autre cas de figure. Vous voulez connaître en direct le nombre de places vendues dans la journée. Vous avez une variable qui contient le nombre actuel de places vendues, et vous voulez lui ajouter
+ le nombre de places vendues à l’instant dans le cinéma.
+Vous écrivez donc */
+let placesDejaVendues = 150
+placesDejaVendues = placesDejaVendues + 10
+/* Comme vous pouvez le constater, j’ai répété deux fois 
+placesDejaVendues. Si j’avais écrit placesDejaVendues = 10, cela aurait écrasé la valeur 150 au lieu de réaliser une addition. Comme ce genre d’opération est très fréquent, JavaScript a créé des raccourcis
+ pour les réaliser : les opérateurs d’affectation.*/
+
+let placesDejaVendues = 150
+placesDejaVendues += 10
+
+/*la premiere etape en javascript*/
+const entreesPremiereSemaine = 1000
+const entreesDeuxiemeSemaine = 2000
+const totalEntrees = entreesPremiereSemaine + entreesDeuxiemeSemaine
+
+console.log(totalEntrees)
+
+/*la deuxieme etape en javascript*/
+let placesDejaVendues = 150
+placesDejaVendues = placesDejaVendues + 10
+console.log(placesDejaVendues)
+
+/* la troisieme etape en javascript */
+let placesDejaVendues = 150
+placesDejaVendues += 10
+
+console.log(placesDejaVendues)
+/*la quatrieme etape en javascript est la premiere essaie  pour les lettres affiches*/
+let monTitre = "Le titre de mon article"
+let monContenu = 'Le contenu de mon article'
+
+let premierePartie = "Mon nom est Bond"
+let deuxiemePartie = ", James Bond."
+let punchline = premierePartie + deuxiemePartie
+console.log(punchline)
+// punchline vaut “Mon nom est Bond, James Bond.”
+
+let punchline = "Mon nom est Bond"
+punchline += ", James Bond."
+console.log(punchline)
+
+let monPersonnage = {
+    nom: "Wayne",
+    prenom: "Bruce",
+    age: 35,
+    couleurPreferee: "noir",
+    hobbies: "sortir la nuit"
+    }
+    monPersonnage.vehiculePrefere = "Batmobile"
+    const nomDeMonPersonnage = monPersonnage.nom
+
+    // Vérification
+    console.log(nomDeMonPersonnage)
+    console.log(monPersonnage.nom)
+// punchline vaut “Mon nom est Bond, James Bond.”
+const MonPays = {
+    nom : "kava",
+    prix : 10,
+    NumeroSalle : 3,
+  }
+  const bar =MonPays.NumeroSalle
+  console.log(bar)
+  //c'est l'exercice simple//
+
+  const MonPays={
+    NomDupays:"burundi",
+    NomProvince:"Gitega",
+    NumeroDeMaison:10,
+    CodeDuPays:+257,
+  }
+  let nom="buja";
+  console.log(MonPays)
+  console.log(nom)
+//il faut detailler ici 
+  const ticket={
+    nomFilm:"Batman",
+    prix:10,
+    numeroSalle:3
+  }
+  let nom="David"
+  //Bonjour NOM, votre film NOMDEFILM est en salle NUMERO DE LA SALLE
+  let textaffiche ="Bonjour " + nom + ",votrefilm " + ticket.nomFilm + " est en salle " + ticket.numeroSalle
+  console.log(textaffiche)
+
+
+  const MonPersonnage={
+    Nom : "Bruce",
+    Prenom : "Wayne",
+    Age : "35"
+  }
+  MonPersonnage.nom="Ornella"
+  console.log(MonPersonnage)
+/*pour verifier l'utilisateur qu'on a deja connecte .*/
+let connexionOk = false
+console.log(connexionOk)
+
+/*qu'on utiliser un guillement en chiffre*/
+let placesDejaVendues = "150"
+placesDejaVendues += "10"
+console.log(placesDejaVendues)
+
+/*ici on utilise number il faut calculer la valeur on a donne la solution*/
+let placesDejaVendues = Number("150")
+placesDejaVendues += Number("10")
+console.log(placesDejaVendues)
+
+//c'est les accolades
+const monCollectionDeFilms = ["Titanic", "Jurassic Park", "le Seugneur des Anneaux"]
+console.log(monCollectionDeFilms)
+//c'est les accolades
+monCollectionDeFilm = ["Titanic", "Jurrasic Park", "Seigneur les Anneaux"]
+const monFilmPrefere = "Titanic"
+const monPremierFilm = "le Seigneur les Anneaux"
+const monCollectionDeFilms = [monFilmPrefere, monPremierFilm]
+console.log(monCollectionDeFilms)
+//c'est les accolades
+monCollectionDeFilm = ["Titanic", "Jurrasic Park", "Seigneur les Anneaux"]
+const monFilmPrefere = "Titanic"
+const monPremierFilm = "le Seigneur les Anneaux"
+const monCollectionDeFilms = [monFilmPrefere, monPremierFilm]
+let premierFilmDuTableau = monCollectionDeFilms[0]
+console.log(monCollectionDeFilms)
+// nombreDeFilms vaut 2
+const maCollectionDeFilms = ["Titanic", "Le Seigneur des Anneaux"]
+const nombreDeFilms = maCollectionDeFilms.length
+console.log(nombreDeFilms)
+// mesFilms vaut ["Titanic", "Jurassic Park", "Retour vers le Futur"]
+let mesFilms = ["Titanic", "Jurassic Park"]
+mesFilms.push("Retour vers le futur")
+
+console.log(mesFilms)
+// mesFilms vaut ["Titanic", "Jurassic Park"]
+let mesFilms = ["Titanic", "Jurassic Park", "Retour vers le futur"]
+mesFilms.pop()
+console.log(mesFilms)
+
+// Copie par valeur
+let variableSimple1 = 25
+let variableSimple2 = variableSimple1
+console.log(variableSimple2)
+
+////////////////////
+// Copie par valeur
+////////////////////
+let variableSimple1 = 25
+let variableSimple2 = variableSimple1
+
+variableSimple2 = 30
+
+// Le console.log va afficher 25, le fait d’avoir changé la valeur de variableSimple2 ne change rien pour variableSimple1
+console.log("variableSimple1", variableSimple1)
+console.log("variableSimple2", variableSimple2)
+
+///////////////////////
+// Copie par référence
+///////////////////////
+let variableComplexe1 = ['pomme', 'cerise']
+let variableComplexe2 = variableComplexe1
+let variableComplexe3 = [...variableComplexe1];
+
+variableComplexe2.push('poire')
+
+// Le console.log va afficher pomme cerise ET poire. On a modifié la seconde variable, mais le contenu de la première a été changé aussi, parce que c’est le même contenu.
+console.log('variableComplexe1', variableComplexe1)
+console.log('variableComplexe2', variableComplexe2)
+console.log('variableComplexe3', variableComplexe3)
+
+
+
+
