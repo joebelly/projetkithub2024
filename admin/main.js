3981531d3f4b482f53c7ff8cf225a4986e009a56
travail2 <lievinira8@gmail.com>
2024-11-12 17:15:09 +0200

    corr

---
+++ admin/main.js
@@ -0,0 +1,19 @@
+let inputEcrire = document.getElementById("inputEcrire")
+console.log(inputEcrire)
+
+let btnValiderMots = document.getElementById("btnValiderMots")
+console.log(btnValiderMots)
+
+let divzoneProposition = document.querySelector(".zoneProposition")
+console.log(divzoneProposition)
+
+let zonespan = document.querySelector(".zoneSaisie")
+console.log(zonespan)
+
+let listeRadio = document.querySelectorAll(".optionSource")
+console.log(listeRadio)
+
+// les commande sur l'image
+let image = document.getElementById("premiereImage");
+//Image.setAttribute("alt", "Ceci est la nouvelle valeur de alt");
+image.src = "img/image-product-desktop.jpg"
